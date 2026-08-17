5fab51836bae79c0d0592cba8feb24a20f6eac14
ShengHaozz <shenghaoong@gmail.com>
2026-08-17 22:45:03 +0800

    Add Deadline class

---
+++ src/main/java/tasks/Deadline.java
@@ -0,0 +1,18 @@
+package tasks;
+
+public class Deadline extends Task {
+    String deadline;
+
+    public Deadline(String name, String deadline) {
+        super(name);
+        this.deadline = deadline;
+    }
+
+    public String getEntryString() {
+        return String.format(
+            "[D]%s (by: %s)",
+            super.getEntryString(),
+            this.deadline
+        );
+    }
+}
