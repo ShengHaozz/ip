449a6c5e6fc6164421643a9969563af2e02c8412
ShengHaozz <shenghaoong@gmail.com>
2026-08-17 21:17:51 +0800

    Add user greeting and exit

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,5 +1,11 @@
 public class Bob {
     public static void main(String[] args) {
-        
+        String horiLines = "_".repeat(30);
+        System.out.println(horiLines);
+        System.out.println("Hello! I'm Bob.");
+        System.out.println("What can I do for you?");
+        System.out.println(horiLines);
+        System.out.println("Goodbye.");
+        System.out.println(horiLines);
     }
 }
