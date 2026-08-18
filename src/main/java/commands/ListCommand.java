5d8bb33d6e806cd925b3f17e488b4376ef76fd05
ShengHaozz <shenghaoong@gmail.com>
2026-08-18 10:56:46 +0800

    Add ListCommand

---
+++ src/main/java/commands/ListCommand.java
@@ -0,0 +1,24 @@
+package commands;
+
+import java.util.ArrayList;
+
+import tasks.Task;
+
+public class ListCommand extends Command {
+    public ListCommand(ArrayList<Task> taskList) {
+        super(taskList);
+    }
+
+    public void processInput(String input) {
+        if (input.equals("list")) {
+            System.out.println("Tasks:");
+            for (int i = 0; i < this.taskList.size(); i++) {
+                System.out.println(String.format(
+                    "%d: %s",
+                    i + 1,
+                    this.taskList.get(i)    
+                ));
+            }
+        }
+    }
+}
