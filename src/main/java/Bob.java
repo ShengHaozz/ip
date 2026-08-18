c071c958b342cfde71773e8ac55525dbb4fcb1a7
ShengHaozz <shenghaoong@gmail.com>
2026-08-18 09:35:40 +0800

    Extract checkCommand out of main

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 import tasks.*;
 
 public class Bob {
+    private static Task[] list = new Task[100];
+    private static int listPtr = 0;
+    private static String horiLines = "_".repeat(30);
     public static void main(String[] args) {
-        String horiLines = "_".repeat(30);
         System.out.println(horiLines);
         System.out.println("Hello! I'm Bob.");
         System.out.println("What can I do for you?");
         System.out.println(horiLines);
 
         Scanner sc = new Scanner(System.in);
-        Task[] list = new Task[100];
-        int listPtr = 0;
 
         while (sc.hasNextLine()) {
             String nextLine = sc.nextLine();
@@ -30,57 +30,64 @@ public class Bob {
                 System.out.println(horiLines);
                 continue;
             }  
-            String[] parts = nextLine.split(" ", 2);
-            String cmd = parts[0];
-            String arg = parts[1];
-            switch (cmd) {
-                case "mark":
-                case "unmark":
-                    int taskId = Integer.parseInt(arg);
-                    if (taskId > listPtr) {
-                        System.out.println("No such task");
-                        System.out.println(horiLines);
-                        continue;
-                    }
-                    Task task = list[taskId - 1];
-                    
-                    if (cmd.equals("mark")) {
-                        task.mark();
-                        System.out.println("Marked as done:");
-                    } else {
-                        task.unmark();
-                        System.out.println("Marked as not done:");
-                    }
-                    
-                    System.out.println(task.getEntryString());
-                    break;
-                
-                case "todo":
-                    ToDo todo = new ToDo(arg);
-                    list[listPtr++] = todo;    
-                    printAddition(todo, listPtr);
-                    break;
-                
-                case "deadline":
-                    String[] deadlineParts = arg.split(" /by ");
-                    Deadline deadline = new Deadline(deadlineParts[0], deadlineParts[1]);
-                    list[listPtr++] = deadline;
-                    printAddition(deadline, listPtr);
-                    break;
-                
-                case "event":
-                    String[] eventParts = arg.split(" /from | /to ");
-                    Event event = new Event(eventParts[0], eventParts[1], eventParts[2]);
-                    list[listPtr++] = event;
-                    printAddition(event, listPtr);
-                    break;
-
-                default:
-                    System.out.println("What's that??");
-                
-                    
+            try {
+                checkCommand(nextLine);
+            } catch (BobException e) {
+                System.out.println(e.getMessage());
             }
             System.out.println(horiLines);
+        }
+    }
+
+    private static void checkCommand(String input) throws BobException{
+        String[] parts = input.split(" ", 2);
+        if (parts.length < 2) {
+            throw new BobException("Error: argument needed for " + parts[0]);
+        }
+        String command = parts[0];
+        String arg = parts[1];
+        switch (command) {
+            case "mark":
+            case "unmark":
+                int taskId = Integer.parseInt(arg);
+                if (taskId > listPtr) {
+                    throw new BobException("Error: Task not found");
+                }
+                Task task = list[taskId - 1];
+                
+                if (command.equals("mark")) {
+                    task.mark();
+                    System.out.println("Marked as done:");
+                } else {
+                    task.unmark();
+                    System.out.println("Marked as not done:");
+                }
+                
+                System.out.println(task.getEntryString());
+                break;
+            
+            case "todo":
+                ToDo todo = new ToDo(arg);
+                list[listPtr++] = todo;    
+                printAddition(todo, listPtr);
+                break;
+            
+            case "deadline":
+                String[] deadlineParts = arg.split(" /by ");
+                Deadline deadline = new Deadline(deadlineParts[0], deadlineParts[1]);
+                list[listPtr++] = deadline;
+                printAddition(deadline, listPtr);
+                break;
+            
+            case "event":
+                String[] eventParts = arg.split(" /from | /to ");
+                Event event = new Event(eventParts[0], eventParts[1], eventParts[2]);
+                list[listPtr++] = event;
+                printAddition(event, listPtr);
+                break;
+
+            default:
+                System.out.println("What's that??");
         }
     }
 
